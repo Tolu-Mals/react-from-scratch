10d345fdad4f74bb752d2df035043e2b0eebc2e1
Tolu-Mals <malomo.tolz@gmail.com>
2022-10-27 18:37:26 +0100

    first commit

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloCompontent = () => {
+  return <h1>Hello World</h1>
+};
+
+ReactDOM.render(<HelloCompontent />, document.getElementById('root'));
